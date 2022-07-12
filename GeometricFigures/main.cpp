6b536c633c79b52e3938874359af673f7464aadd
Andrei <ikar2008@hotmail.com>
2022-07-12 17:31:31 +0300

    Constructor and Destructor Rectangle and Square

--- GeometricFigures/main.cpp
+++ GeometricFigures/main.cpp
@@ -40,13 +40,24 @@ public:
 	{
 		this->diagonal = diagonal;
 	}
+
+	Square()
+	{
+		this->side = 0;
+		this->diagonal = 0;
+		cout << "DefaultConstructor:\t" << this << endl;
+	}
+	~Square()
+	{
+		cout << "Destructor: \t\t" << this << endl;
+	}
 };
 
 class Rectangle
 {
-	double width;
-	double height;
-	double diagonal;
+	float width;
+	float height;
+	//float diagonal;
 
 public:
 	double get_width()const
@@ -57,25 +68,45 @@ public:
 	{
 		return height;
 	}
-	double get_diagonal()const
+	/*double get_diagonal()const
 	{
 		return diagonal;
-	}
+	}*/
 
-	void set_width(double width)
+	void set_width(float width)
 	{
 		this->width = width;
 	}
 	
-	void set_height(double height)
+	void set_height(float height)
 	{
 		this->height = height;
 	}
 
-	void set_diagonal(double diagonal)
+	/*void set_diagonal(double diagonal)
 	{
 		this->diagonal = diagonal;
+	}*/
+
+	void get()
+	{
+		cout << "Высота: " << height << "\nШирина: " << width << endl;
 	}
+
+	Rectangle()
+	{
+		this->width = 0;
+		this->height = 0;
+		cout << "DefaultConstructor:\t" << this << endl;
+	}
+	~Rectangle()
+	{
+		cout << "Destructor: \t\t" << this << endl;
+	}
+	void Perimeter()
+	{}
+	void Square()
+	{}
 
 };
 
