381512949287a5ed2578149f641d0f14bf54736a
Andrei <ikar2008@hotmail.com>
2022-07-12 16:26:12 +0300

    Start HomeWork GeometricFigures

---
+++ GeometricFigures/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+void main()
+
+{
+	setlocale(LC_ALL, "");
+
+}
